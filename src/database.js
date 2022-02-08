8a6b6764ffef8f0c03e7a914e4f0d13d62360017
Jihee <hollister7856@gmail.com>
2022-02-08 23:32:37 +0900

    Connect oracledb

---
+++ src/database.js
@@ -0,0 +1,30 @@
+import oracledb from "oracledb";
+
+oracledb.getConnection({
+    user          : "jihee",
+    password      : "64605548",
+    connectString : "localhost/XE"
+}, function (err, conn) {
+    if(err){
+        console.log('접속 실패', err);
+        return;
+    }
+    console.log('접속 성공');
+});
+
+conn.execute("select * from member", {}, {outFormat:oracledb.OBJECT}, function (err, result) {  
+    // Json 형태로 넘어오도록 설정
+if(err) throw err; 
+
+console.log('query read success');
+
+dataStr = JSON.stringify(result);
+console.log(dataStr);
+
+arrStr = JSON.stringify(result.rows);
+var arr = JSON.parse(arrStr);
+console.log(arr);
+
+console.log(arr[0].ID + " " + arr[0].PASSWORD);
+
+});
