8a8cfd90d6a7add96d77a9980a4dd247184b5718
hollister8 <hollister7856@gmail.com>
2022-06-20 17:19:56 +0900

    write 페이지 post 전송

--- client/src/components/Write.tsx
+++ client/src/components/Write.tsx
@@ -1,12 +1,24 @@
+import axios from "axios";
 import React from "react";
 
 export default function Write() {
+    axios.post("http://localhost:8081/upload", {
+        title: "",
+        content: ""
+    })
+    .then(function (response) {
+        console.log(response)
+    }).catch(function (error) {
+        console.log(error)
+    }).then(function() {
+        // 항상 실행
+    });
 
     return (
-        <div className="">
-            <form action="">
-                <input type="text" placeholder="제목" />
-                <textarea name="" id="" placeholder="내용"></textarea>
+        <div className="wirte-container">
+            <form action="/upload" method="post">
+                <div><input type="text" name="title" placeholder="제목" required /></div>
+                <div><textarea name="content" placeholder="내용" required></textarea></div>
                 <button type="submit">글쓰기</button>
             </form>
         </div>
