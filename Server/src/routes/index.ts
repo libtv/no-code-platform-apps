5c23e896d076779b84424fc8e03d4cbabe563b9f
Junho <libtv@naver.com>
2022-07-27 11:09:34 +0900

    로그 일부분 수정

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -2,6 +2,10 @@ import { FILE_SEPARATOR, IMPORT_API_EXTENSION } from "./../const/const";
 import express from "express";
 import { getDirectory, importDirectory } from "../utils/path";
 import errorRouter from "./error/error.assembly";
+import { logger } from "../utils/logger";
+import path from "path";
+
+const jsName = path.basename(__filename);
 
 async function setApi(app: express.Router) {
     const apiDirectory = getDirectory("routes/api");
@@ -9,12 +13,14 @@ async function setApi(app: express.Router) {
         apiDirectory.map(async ({ dir, api }, idx) => {
             let router = await importDirectory(dir + FILE_SEPARATOR + api + "." + IMPORT_API_EXTENSION);
             app.use(router.default("api", api));
+            logger.info(`[${jsName}] - api/${api} 의 라우터가 추가되었습니다.`);
         })
     );
 }
 
 async function setError(app: express.Router) {
     app.use(errorRouter);
+    logger.info(`[${jsName}] - 에러 라우터가 추가되었습니다.`);
 }
 
 export async function setRoutes(app: express.Router) {
